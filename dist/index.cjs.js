b98f1621f6205d26f1411a16619205a7631b1679
Luca Ban - Black Birdy <mesqueeb@gmail.com>
2018-09-11 11:27:41 +0900

    Initial commit 🏄🏼‍♂️

---
+++ dist/index.cjs.js
@@ -0,0 +1,30 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var isWhat = require('is-what');
+
+/**
+ * Goes through an object recursively and replaces all occurences of the `find` value with `replaceWith`. Also works no non-objects.
+ *
+ * @export
+ * @param {*} target Target can be anything
+ * @param {*} find val to find
+ * @param {*} replaceWith val to replace
+ * @returns the target with replaced values
+ */
+
+function findAndReplaceRecursively(target, find, replaceWith) {
+  if (!isWhat.isObject(target)) {
+    if (target === find) return replaceWith;
+    return target;
+  }
+
+  return Object.keys(target).reduce(function (carry, key) {
+    var val = target[key];
+    carry[key] = findAndReplaceRecursively(val, find, replaceWith);
+    return carry;
+  }, {});
+}
+
+exports.default = findAndReplaceRecursively;
