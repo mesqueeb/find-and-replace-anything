548e2bd1c0ea46767b555ae28a5e261b2c658b76
Mesqueeb <mesqueeb@gmail.com>
2020-03-15 13:24:31 +0900

    update dependencies 🌝

--- test/index.ts
+++ test/index.ts
@@ -1,11 +1,11 @@
 import test from 'ava'
-import {findAndReplace, findAndReplaceIf} from '../dist/index.cjs'
+import { findAndReplace, findAndReplaceIf } from '../src/index'
 
 test('findAndReplace', t => {
   let res, ori
-  ori = {a: {b: {c: 'a'}}}
+  ori = { a: { b: { c: 'a' } } }
   res = findAndReplace(ori, 'a', 'b')
-  t.deepEqual(res, {a: {b: {c: 'b'}}})
+  t.deepEqual(res, { a: { b: { c: 'b' } } })
   res = findAndReplace('_', 'a', 'b')
   t.is(res, '_')
   res = findAndReplace('a', 'a', 'b')
@@ -14,27 +14,27 @@ test('findAndReplace', t => {
 
 test('findAndReplace does not modify objects', t => {
   let res, ori
-  ori = {a: {b: {c: 'a'}, d: 1}}
+  ori = { a: { b: { c: 'a' }, d: 1 } }
   res = findAndReplace(ori, 'a', 'b')
-  t.deepEqual(res, {a: {b: {c: 'b'}, d: 1}})
-  t.deepEqual(ori, {a: {b: {c: 'a'}, d: 1}})
+  t.deepEqual(res, { a: { b: { c: 'b' }, d: 1 } })
+  t.deepEqual(ori, { a: { b: { c: 'a' }, d: 1 } })
   res.a.b = 1
-  t.deepEqual(res, {a: {b: 1, d: 1}})
-  t.deepEqual(ori, {a: {b: {c: 'a'}, d: 1}})
+  t.deepEqual(res, { a: { b: 1, d: 1 } })
+  t.deepEqual(ori, { a: { b: { c: 'a' }, d: 1 } })
   res.a.d = 2
-  t.deepEqual(res, {a: {b: 1, d: 2}})
-  t.deepEqual(ori, {a: {b: {c: 'a'}, d: 1}})
+  t.deepEqual(res, { a: { b: 1, d: 2 } })
+  t.deepEqual(ori, { a: { b: { c: 'a' }, d: 1 } })
   ori.a.d = 3
-  t.deepEqual(res, {a: {b: 1, d: 2}})
-  t.deepEqual(ori, {a: {b: {c: 'a'}, d: 3}})
+  t.deepEqual(res, { a: { b: 1, d: 2 } })
+  t.deepEqual(ori, { a: { b: { c: 'a' }, d: 3 } })
 })
 
 test('findAndReplace does not work with objects', t => {
   let res, ori
-  ori = {a: {b: {c: 'a'}}}
-  res = findAndReplace(ori, {c: 'a'}, {c: 'b'})
-  t.deepEqual(res, {a: {b: {c: 'a'}}})
-  t.deepEqual(ori, {a: {b: {c: 'a'}}})
+  ori = { a: { b: { c: 'a' } } }
+  res = findAndReplace(ori, { c: 'a' }, { c: 'b' })
+  t.deepEqual(res, { a: { b: { c: 'a' } } })
+  t.deepEqual(ori, { a: { b: { c: 'a' } } })
 })
 
 test('findAndReplaceIf', t => {
@@ -43,8 +43,8 @@ test('findAndReplaceIf', t => {
     if (foundVal === 'a') return 'b'
     return foundVal
   }
-  res = findAndReplaceIf({a: {b: {c: 'a'}}}, checkFn)
-  t.deepEqual(res, {a: {b: {c: 'b'}}})
+  res = findAndReplaceIf({ a: { b: { c: 'a' } } }, checkFn)
+  t.deepEqual(res, { a: { b: { c: 'b' } } })
   res = findAndReplaceIf('_', checkFn)
   t.is(res, '_')
   res = findAndReplaceIf('a', checkFn)
@@ -54,6 +54,7 @@ test('findAndReplaceIf', t => {
 test('should work on classes', t => {
   let res, target
   class MyClass {
+    prop = 0
     constructor () {
       this.prop = 1
     }
@@ -61,7 +62,7 @@ test('should work on classes', t => {
   const myClass = new MyClass()
   target = {
     prop: 1,
-    class: myClass
+    class: myClass,
   }
   res = findAndReplace(target, 1, 2)
   t.is(res.prop, 2)
@@ -71,6 +72,7 @@ test('should work on classes', t => {
 test('should prevent classes', t => {
   let res, target
   class MyClass {
+    prop = 0
     constructor () {
       this.prop = 1
     }
@@ -78,9 +80,9 @@ test('should prevent classes', t => {
   const myClass = new MyClass()
   target = {
     prop: 1,
-    class: myClass
+    class: myClass,
   }
-  res = findAndReplace(target, 1, 2, {onlyPlainObjects: true})
+  res = findAndReplace(target, 1, 2, { onlyPlainObjects: true })
   t.is(res.prop, 2)
   t.is(res.class.prop, 1)
 })
